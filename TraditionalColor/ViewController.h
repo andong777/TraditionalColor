a2fcb0f7b9178845306155d6e357909cb7c56e20
andong777 <andong777@163.com>
2015-08-14 11:22:56 +0800

    Initial Commit

---
+++ TraditionalColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TraditionalColor
+//
+//  Created by andong on 15/8/14.
+//  Copyright (c) 2015年 AN Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TraditionalColor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TraditionalColor
+//
+//  Created by andong on 15/8/14.
+//  Copyright (c) 2015年 AN Dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
